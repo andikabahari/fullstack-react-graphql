3bfb6047f9228a87dfbc15a5141e38c69ce9b904
Andika Bahari <kzlfxx@gmail.com>
2022-02-19 20:19:02 +0700

    Setup Apollo Server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
-  // const post = orm.em.fork({}).create(Post, {
-  //   title: "First Post",
-  //   createdAt: new Date(),
-  //   updatedAt: new Date(),
-  // });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const port = 5000;
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 };
 
 main().catch((err) => console.log(err));
