c3dda8335b4b8cd40dbf1e685ab0e4b1030c8d73
Andika Bahari <kzlfxx@gmail.com>
2022-03-01 17:04:58 +0700

    Add form to change password

--- client/src/pages/change-password/[token].tsx
+++ client/src/pages/change-password/[token].tsx
@@ -1,11 +1,46 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
 import { NextPage } from "next";
+import router from "next/router";
+import InputField from "../../components/InputField";
+import Wrapper from "../../components/Wrapper";
+import { toErrorMap } from "../../utils/toErrorMap";
+import login from "../login";
 
 interface ChangePasswordProps {
   token: string;
 }
 
 const ChangePassword: NextPage<ChangePasswordProps> = ({ token }) => {
-  return <div>token is {token}</div>;
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          //
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="newPassword"
+              label="New Password"
+              placeholder="New Password"
+              type="password"
+            />
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              Change password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 ChangePassword.getInitialProps = ({ query }) => {
