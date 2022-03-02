90836cab74f1a103c7f4453a1bc58d223a9a1847
Andika Bahari <kzlfxx@gmail.com>
2022-03-02 11:16:54 +0700

    Add forgot password page

--- client/src/pages/change-password/[token].tsx
+++ client/src/pages/change-password/[token].tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Link } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import { NextPage } from "next";
 import { withUrqlClient } from "next-urql";
@@ -9,6 +9,7 @@ import Wrapper from "../../components/Wrapper";
 import { useChangePasswordMutation } from "../../generated/graphql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 import { toErrorMap } from "../../utils/toErrorMap";
+import NextLink from "next/link";
 
 interface ChangePasswordProps {
   token: string;
@@ -40,7 +41,14 @@ const ChangePassword: NextPage<ChangePasswordProps> = ({ token }) => {
               placeholder="New Password"
               type="password"
             />
-            {tokenError ? <Box color="red">{tokenError}</Box> : null}
+            {tokenError ? (
+              <Box>
+                <Box style={{ color: "red" }}>{tokenError}</Box>
+                <NextLink href="forgot-password">
+                  <Link>Forgot your password?</Link>
+                </NextLink>
+              </Box>
+            ) : null}
             <Button
               mt={4}
               isLoading={isSubmitting}
---
+++ client/src/pages/forgot-password.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ForgotPasswordProps {}
+
+const ForgotPassword: React.FC<ForgotPasswordProps> = ({}) => {
+  return <div>forgot password page</div>;
+};
+
+export default ForgotPassword;
