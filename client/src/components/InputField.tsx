11e03deddd78ef63de5bb894afe9168f0968a612
Andika Bahari <kzlfxx@gmail.com>
2022-03-03 12:24:16 +0700

    Handle form submit

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -3,25 +3,31 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
 
-type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+type InputFieldProps = InputHTMLAttributes<
+  HTMLInputElement & HTMLTextAreaElement
+> & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = textarea ? Textarea : Input;
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
+      <InputOrTextarea
         {...field}
         {...props}
         id={field.name}
--- client/src/pages/create-post.tsx
+++ client/src/pages/create-post.tsx
@@ -1,25 +1,37 @@
-import { Box, Flex, Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
 import React from "react";
 import InputField from "../components/InputField";
 import Wrapper from "../components/Wrapper";
+import { useCreatePostMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 interface CreatePostProps {}
 
-export const CreatePost: React.FC<CreatePostProps> = ({}) => {
+const CreatePost: React.FC<CreatePostProps> = ({}) => {
+  const router = useRouter();
+  const [, createPost] = useCreatePostMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ title: "", text: "" }}
         onSubmit={async (values, { setErrors }) => {
-          console.log(values);
+          await createPost({ input: values });
+          router.push("/");
         }}
       >
         {({ isSubmitting }) => (
           <Form>
             <InputField name="title" label="Title" placeholder="Title" />
             <Box mt={4}>
-              <InputField name="text" label="Body" placeholder="Text..." />
+              <InputField
+                name="text"
+                label="Body"
+                placeholder="Text..."
+                textarea={true}
+              />
             </Box>
             <Button
               mt={4}
@@ -35,3 +47,5 @@ export const CreatePost: React.FC<CreatePostProps> = ({}) => {
     </Wrapper>
   );
 };
+
+export default withUrqlClient(createUrqlClient)(CreatePost);
