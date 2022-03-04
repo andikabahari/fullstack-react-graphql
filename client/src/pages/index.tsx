68b2a2a03ed911c215421a8fee90ffd075933870
Andika Bahari <kzlfxx@gmail.com>
2022-03-04 15:07:32 +0700

    Apply styling on posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { Box, Heading, Stack, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
+import Layout from "../components/Layout";
 import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
@@ -6,15 +8,20 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 const Index = () => {
   const [{ data }] = usePostsQuery({ variables: { limit: 10 } });
   return (
-    <>
-      <NavBar></NavBar>
-      <div>Hello, World!</div>
+    <Layout>
       {data ? (
-        data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+        <Stack spacing={8}>
+          {data.posts.map((post) => (
+            <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.text.slice(0, 100)}</Text>
+            </Box>
+          ))}
+        </Stack>
       ) : (
         <div>loading...</div>
       )}
-    </>
+    </Layout>
   );
 };
 
