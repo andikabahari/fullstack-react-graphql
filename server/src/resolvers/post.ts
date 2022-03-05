640c2b183f732cbfaef0f450af633bbb09ce0712
Andika Bahari <kzlfxx@gmail.com>
2022-03-05 15:02:26 +0700

    Fetching posts with relationship

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -4,6 +4,7 @@ import {
   Ctx,
   Field,
   FieldResolver,
+  Info,
   InputType,
   Int,
   Mutation,
@@ -46,20 +47,30 @@ export class PostResolver {
   @Query(() => PaginatedPosts)
   async posts(
     @Arg("limit", () => Int) limit: number,
-    @Arg("cursor", () => String, { nullable: true }) cursor: string
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
-    const query = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("post")
-      .orderBy('"createdAt"', "DESC")
-      .take(realLimit + 1);
+    const replacements: any[] = [realLimit + 1];
     if (cursor) {
-      query.where('"createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor)),
-      });
+      replacements.push(new Date(parseInt(cursor)));
     }
-    const posts = await query.getMany();
+    const posts = await getConnection().query(
+      `
+        select p.*, json_build_object(
+          'id', u.id,
+          'username', u.username,
+          'email', u.email,
+          'createdAt', u."createdAt",
+          'updatedAt', u."updatedAt"
+        ) creator
+        from post p
+        inner join public.user u on u.id = p."creatorId"
+        ${cursor ? `where p."createdAt" < $2` : ""}
+        order by p."createdAt" DESC
+        limit $1
+    `,
+      replacements
+    );
     return {
       posts: posts.slice(0, realLimit),
       hasMore: posts.length === realLimit + 1,
