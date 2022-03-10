3134dcd7c631dcc1b6de018720c1b05e3a7c3c46
Andika Bahari <kzlfxx@gmail.com>
2022-03-10 20:09:24 +0700

    Link all posts to their id

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -42,7 +42,11 @@ const Index = () => {
                 <UpvoteSection post={post} />
               </Box>
               <Box>
-                <Heading fontSize="xl">{post.title}</Heading>
+                <NextLink href="/post/[id]" as={`/post/${post.id}`}>
+                  <Link>
+                    <Heading fontSize="xl">{post.title}</Heading>
+                  </Link>
+                </NextLink>
                 <Text>Posted by {post.creator.username}</Text>
                 <Text mt={4}>{post.textSnippet}</Text>
               </Box>
