25c4d28fc80b5c43e08fb24067a477ed5358a92d
Andika Bahari <kzlfxx@gmail.com>
2022-03-11 21:02:31 +0700

    Add GraphQL mutation to delete post

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -148,7 +148,7 @@ export class PostResolver {
   @Mutation(() => Boolean)
   @UseMiddleware(isAuth)
   async deletePost(
-    @Arg("id") id: number,
+    @Arg("id", () => Int) id: number,
     @Ctx() { req }: MyContext
   ): Promise<boolean> {
     await Post.delete({ id, creatorId: req.session.userId });
