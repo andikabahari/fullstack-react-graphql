cacec7fb4ae99316c5358604f7baa0f81bd222a9
Andika Bahari <kzlfxx@gmail.com>
2022-03-13 20:42:34 +0700

    Reset cache after login & logout

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -4,7 +4,7 @@ import {
   fetchExchange,
   stringifyVariables,
 } from "urql";
-import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
+import { cacheExchange, Resolver, Cache } from "@urql/exchange-graphcache";
 import {
   LoginMutation,
   MeQuery,
@@ -69,6 +69,14 @@ export const cursorPagination = (): Resolver => {
   };
 };
 
+const invalidateAllPosts = (cache: Cache) => {
+  const allFields = cache.inspectFields("Query");
+  const fieldInfos = allFields.filter((info) => info.fieldName === "posts");
+  fieldInfos.forEach((info) => {
+    cache.invalidate("Query", "posts", info.arguments);
+  });
+};
+
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = "";
   if (isServer()) {
@@ -102,6 +110,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
                 (result, query) =>
                   result.login.errors ? query : { me: result.login.user }
               );
+              invalidateAllPosts(cache);
             },
             logout: (_result: LogoutMutation, args, cache, info) => {
               myUpdateQuery<LogoutMutation, MeQuery>(
@@ -121,13 +130,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
               );
             },
             createPost: (_result, args, cache, info) => {
-              const allFields = cache.inspectFields("Query");
-              const fieldInfos = allFields.filter(
-                (info) => info.fieldName === "posts"
-              );
-              fieldInfos.forEach((info) => {
-                cache.invalidate("Query", "posts", info.arguments);
-              });
+              invalidateAllPosts(cache);
             },
             vote: (_result, args, cache, info) => {
               const { postId, value } = args as VoteMutationVariables;
