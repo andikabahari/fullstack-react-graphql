f77bd20da341b6bc95af54750a68998ab84b5b1f
Andika Bahari <kzlfxx@gmail.com>
2022-03-13 18:43:52 +0700

    Add PostButtons component

---
+++ client/src/components/PostButtons.tsx
@@ -0,0 +1,35 @@
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import React from "react";
+import { Flex, IconButton, Link } from "@chakra-ui/react";
+import { useDeletePostMutation, useMeQuery } from "../generated/graphql";
+import NextLink from "next/link";
+
+interface PostButtonsProps {
+  postId: number;
+  creatorId: number;
+}
+
+export const PostButtons: React.FC<PostButtonsProps> = ({
+  postId,
+  creatorId,
+}) => {
+  const [, deletePost] = useDeletePostMutation();
+  const [{ data: meData }] = useMeQuery();
+
+  if (meData?.me?.id !== creatorId) {
+    return null;
+  }
+
+  return (
+    <Flex alignItems="center">
+      <NextLink href="/post/edit/[id]" as={`/post/edit/${postId}`}>
+        <IconButton as={Link} aria-label="Edit post" icon={<EditIcon />} />
+      </NextLink>
+      <IconButton
+        onClick={() => deletePost({ id: postId })}
+        aria-label="Delete post"
+        icon={<DeleteIcon />}
+      />
+    </Flex>
+  );
+};
