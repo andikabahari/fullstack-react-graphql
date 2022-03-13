eab94a33a8076f708a33e2b44dbffdc78f36691c
Andika Bahari <kzlfxx@gmail.com>
2022-03-13 18:10:09 +0700

    Refactor post page

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -1,26 +1,31 @@
 import { Heading } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
-import { useRouter } from "next/router";
 import React from "react";
 import Layout from "../../components/Layout";
-import { usePostQuery } from "../../generated/graphql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
+import { useGetPost } from "../../utils/useGetPost";
 
 const Post: React.FC<{}> = ({}) => {
-  const router = useRouter();
-  const postId =
-    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
-  const [{ data, fetching }] = usePostQuery({
-    pause: postId === -1,
-    variables: { id: postId },
-  });
+  const [{ data, error, fetching }] = useGetPost();
 
   if (fetching) {
-    return <Layout>loading...</Layout>;
+    return (
+      <Layout>
+        <div>loading...</div>
+      </Layout>
+    );
   }
 
   if (!data?.post) {
-    return <Layout>Could not find post.</Layout>;
+    return (
+      <Layout>
+        <div>Could not find post.</div>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
   }
 
   return (
